ecb39e3765002c3f8a711657afa517309a337c9e
yumaito <yumaito0@gmail.com>
2015-06-13 01:41:51 +0900

    modify soundbyclick.js

--- D2015/lib/soundbyclick.js
+++ D2015/lib/soundbyclick.js
@@ -1,10 +1,36 @@
 $(function(){
 	var soundP = 70;
+	var TsukaSound = ["a-sounan","eee","illusion","sasuga","seki","sindoiwa","soredeka","sugoiyo","uwa-sugoina","wakaran","warai1","warai2"];
+	var TsukaSerifu = ["あ～そうなん","え～","イリュージョン","さすが","ゴホゴホ","しんどいわ","それでか","すごいよ～","うわ～すごいな","わからん","はっはっは","ははは"];
 	$(":radio").change(function(event) {
 		/* Act on the event */
 		var target = $(event.target);
 		//ラジオボタンがチェックされたら
-		if($(target).val() == "3"){
+		switch($(target).val()){
+			case "1":
+			break;
+			case "2":
+			break;
+			case "3":
+			break;
+			case "4":
+			break;
+			case "5":
+			break;
+			default:
+			break;
+		}
+		var voiceflag = TsukaOrTera();
+		if(voiceflag == "Tsukamoto"){
+			var voices = TsukaSound.length;
+			var num = Math.round(Math.random()*voices);
+			soundedVoice = TsukaSound[num];
+			CreateAudio(soundedVoice);
+			var count = 0;
+			sound(count);
+			count = 1;
+		}
+		/*if($(target).val() == "3"){
 			//乱数発生
 			var val = Math.round(Math.random()*100);
 			if(val<=soundP){
@@ -15,18 +41,28 @@ $(function(){
 				sound(count);
 				count =1;
 			}
-		}
+		}*/
 	});
 });
-function CreateAudio(){
+function CreateAudio(name,num){
+	var path = "sound/se/voices/";
+	var dir = "";
+	if(num == 0){
+		dir = "tsukamoto/";
+	}else{
+		dir = "terada/";
+	}
+	//var tsuka = "tsukamoto";
+	//var tera = "terada";
 	var audio = document.createElement("audio");
 	//ID要素を設定
 	audio.id="voice";
+	var location = path + dir + name;
 	//src要素を設定
 	if(audio.canPlayType("audio/mp3")){
-		audio.src="sound/se/voices/naruhodo.mp3";
+		audio.src=location+".mp3";
 	}else if(audio.canPlayType("audio/wav")){
-		audio.src="sound/se/voices/naruhofo.wav";
+		audio.src=location+".wav";
 	}
 	//bodyの最も後ろに[audio]を生成
 	document.body.appendChild(audio);
@@ -36,4 +72,16 @@ function sound(num){
 		document.getElementById("voice").currentTime=0;
 	}
 	$("#voice").get(0).play();
+}
+//塚本先生か寺田先生か鳴らすか鳴らさないかを決める
+function TsukaOrTera(){
+	var val = Math.round(Math.random()*3);
+	switch(val){
+		case 0:
+			return "Tsukamoto";
+		case 1:
+			return "Terada";
+		case 2:
+			return "none";
+	}
 }
