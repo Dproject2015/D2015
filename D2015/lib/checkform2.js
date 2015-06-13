bf6bcf9474a57000ac6c8441feea94239a11b21e
yumaito-mac <yumaito0@gmail.com>
2015-06-13 18:30:11 +0900

    modify

--- D2015/lib/checkform2.js
+++
@@ -1,9 +0,0 @@
-$(function(){
-	$("#additional").attr("disabled","disabled");
-	$("#dbselect").change(function(event){
-		var target = $(event.target);
-		if($(target).val()=="scores"){
-			$("#additional").removeAttr("disabled");
-		}
-	});
-});
